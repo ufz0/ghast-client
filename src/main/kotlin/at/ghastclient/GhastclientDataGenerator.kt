5214c96446a03935698b6ddb9303323033a5544f
ufz0 <l.leutgeb@students.htl-leonding.ac.at>
2025-06-26 09:45:20 +0200

    Initial commit

---
+++ src/main/kotlin/at/ghastclient/GhastclientDataGenerator.kt
@@ -0,0 +1,9 @@
+package at.ghastclient
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object GhastclientDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
