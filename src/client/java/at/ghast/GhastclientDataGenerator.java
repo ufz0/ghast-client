858495a25b5c92381fd428c8159ede857fe425af
ufz0 <l.leutgeb@students.htl-leonding.ac.at>
2025-06-27 19:57:34 +0200

    Converted to java

---
+++ src/client/java/at/ghast/GhastclientDataGenerator.java
@@ -0,0 +1,11 @@
+package at.ghast;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class GhastclientDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
