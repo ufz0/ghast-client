f323449d840c8430dc4dc0a253ee9289d73cb73d
ufz0 <l.leutgeb@students.htl-leonding.ac.at>
2025-07-09 02:53:05 +0200

    Removed useless imports + comment in tickEvents.java

--- src/main/java/at/ghasty/tickEvents.java
+++ src/main/java/at/ghasty/tickEvents.java
@@ -5,12 +5,7 @@ import at.ghasty.notifications.ToastNotification;
 import at.ghasty.screens.modMenuScreen;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.toast.SystemToast;
-import net.minecraft.client.toast.Toast;
 import net.minecraft.text.Text;
-import at.ghasty.globals;
-
-import java.util.Optional;
 
 public class tickEvents {
     public static boolean shownInfo = false;
@@ -28,8 +23,6 @@ public class tickEvents {
             while (keybinds.openUI.wasPressed()) {
                 Ghastclient.LOGGER.info("Opened UI!");
                 if (Ghastclient.client.player != null) {
-                    //Ghastclient.client.player.sendMessage(Text.of("Hello World!"), true);
-
                     Ghastclient.client.setScreen(
                             new modMenuScreen(Text.empty())
                     );
